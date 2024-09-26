8d5a95afb4d60a0c586f2ba17a26ddb1fe6d8795
bochar68 <bochar.dg@gmail.com>
2024-09-26 11:06:11 +0300

    Цикл While

---
+++ module_2_3.py
@@ -0,0 +1,15 @@
+# Нули ничто, отрицание недопустимо!
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+new_list = []
+l = len(my_list)
+l1 = -1
+while l >= l1:
+    l1 +=1
+    if my_list[l1] < 0:
+        break
+    elif my_list[l1] == 0:
+        continue
+    else:
+        print(my_list[l1])
+        new_list.append(my_list[l1])
+print(f'Список составлен:\n {new_list}')
