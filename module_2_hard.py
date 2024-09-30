941a9f411398ffaab207a6242f0c7c6fe73f20ce
bochar68 <bochar.dg@gmail.com>
2024-09-30 14:35:17 +0300

    убрана лишняя проверка

--- module_2_hard.py
+++ module_2_hard.py
@@ -2,8 +2,8 @@
 result = [0] * 21
 for i in range(3,21):
     for j in range(1,i):
-        for k in range(1,i):
-            if k>j and i % (j + k) == 0:
+        for k in range(j+1,i):
+            if i % (j + k) == 0:
                 result[i] = int(str(result[i]) + str(j) + str(k))
 print('Пароли расчитаны.')
 while 3 < 20:
