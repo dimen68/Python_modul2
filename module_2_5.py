19cd0097764344d7cceedd2acfd9defc08584b0f
bochar68 <bochar.dg@gmail.com>
2024-09-30 12:53:03 +0300

    добавлен вывод в виде матрицы

--- module_2_5.py
+++ module_2_5.py
@@ -10,6 +10,9 @@ def get_matrix(n, m, value):
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
-print(result1)
-print(result2)
-print(result3)
+for i in result1:
+    print(*i)
+for j in result2:
+    print(*j)
+for k in result3:
+    print(*k)
